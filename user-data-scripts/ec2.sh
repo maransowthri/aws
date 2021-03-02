d3ea0732f3ad381ebd94631a5f42bb40327364f0
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2021-03-02 13:03:13 +0530

    Added scripts and slides

---
+++ user-data-scripts/ec2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# get admin previleges
+sudo su
+
+# Installing apache
+yum update -y
+yum install -y httpd.x86_64
+systemctl start httpd.service
+systemctl enable httpd.service
+echo "Hello World from $(hostname -f)" > /var/www/html/index.html
